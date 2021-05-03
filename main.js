6598efb609a1662cbdb7926beba80d7cfb9b4fe2
fool1280 <anhnguyen@Anhs-MacBook-Pro.local>
2021-05-03 12:06:16 -0500

    add comments for debug

--- main.js
+++ main.js
@@ -9,15 +9,19 @@ let dict = {
     7: "Seven",
     8: "Eight",
     9: "Nine",
-};
+}; //create a dictionary for better look up and easy access
+
 let res = "";
 for (i = 2; i < process.argv.length; i++) {
+    //first two argument are paths
     let number = process.argv[i];
     for (j = 0; j < number.length; j++) {
         res += dict[number[j]];
+        //look up each single digits
     }
     if (i != process.argv.length - 1) {
         res += ",";
+        //not the last element
     }
 }
 console.log(res);
